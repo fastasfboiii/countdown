8b56c788891c6ebefb2c08923c7b6463441045d4
fastasfboiii <ahmedyasser200373@gmail.com>
2023-07-10 00:37:51 +0800

    final math version

---
+++ script.js
@@ -0,0 +1,46 @@
+const countdownElement = document.getElementById('countdown');
+const quoteContainer = document.getElementById('quote-container');
+const quoteBlocks = quoteContainer.getElementsByTagName('blockquote');
+const quoteAuthor = quoteContainer.querySelector('.quote-author');
+
+let currentQuoteIndex = 0;
+
+function updateCountdown(targetDate) {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countdownElement.innerHTML = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+
+  if (distance > 0) {
+    setTimeout(() => {
+      updateCountdown(targetDate);
+    }, 1000);
+  } else {
+    countdownElement.innerHTML = "Countdown expired";
+  }
+}
+
+function displayNextQuote() {
+  quoteBlocks[currentQuoteIndex].classList.remove('active');
+  currentQuoteIndex = (currentQuoteIndex + 1) % quoteBlocks.length;
+  quoteBlocks[currentQuoteIndex].classList.add('active');
+  quoteAuthor.innerHTML = getQuoteAuthor(quoteBlocks[currentQuoteIndex].textContent);
+}
+
+function getQuoteAuthor(quote) {
+  const startIndex = quote.lastIndexOf('-');
+  if (startIndex !== -1) {
+    return quote.slice(startIndex + 1).trim();
+  }
+  return '';
+}
+
+const targetDate = prompt("Enter the target date (YYYY-MM-DD HH:MM:SS)");
+updateCountdown(new Date(targetDate).getTime());
+displayNextQuote();
+setInterval(displayNextQuote, 10800000); // Change quote every 3 hours (3 hours = 3 * 60 * 60 * 1000 milliseconds)
